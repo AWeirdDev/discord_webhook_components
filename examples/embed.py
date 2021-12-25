3ad3cd19dc87fa44af84bbd8868a41265625cdef
AWeirdScratcher <90096971+AWeirdScratcher@users.noreply.github.com>
2021-12-25 20:51:25 +0800

    embed.py
    This is an example for presenting an embed object

---
+++ examples/embed.py
@@ -0,0 +1,19 @@
+from discord_webhook_components import checkWebhook, embed, send
+import discord
+from discord.ext import commands
+
+client = commands.Bot(command_prefix='!')
+
+@client.event
+async def on_ready():
+  print('ready!')
+  
+@client.command()
+async def embed(ctx):
+  url = await checkWebhook(ctx, 'WEBHOOK_NAME')
+  my_embed = embed(title="My New Embed!!11!", description='Embeds are really nice :D', image="Image URL", thumbnail="Thumbnail URL", color=0x0995ec, footer="Footer text :D")
+  sec_embed = embed(title="Multiple Embeds! Nice!", description="multiple embeds are cool B)", color=0x0995ec) #The color's default value is 0x0995ec
+  send(url=url, content="My first webhoook!", embeds=[my_embed, sec_embed]) #list
+ 
+
+client.run('AMAZING TOKEN')
