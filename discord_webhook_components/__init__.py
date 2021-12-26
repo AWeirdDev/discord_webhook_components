f66bbe6fd614bec1e189dc8c319789d4a4edae0b
AWeirdScratcher <90096971+AWeirdScratcher@users.noreply.github.com>
2021-12-26 18:58:48 +0800

    Create __init__.py

---
+++ discord_webhook_components/__init__.py
@@ -0,0 +1,20 @@
+from .all import button, URLbutton, checkWebhook, checkWebhookToken, send, embed
+
+"""
+discord_webhook_components is a module that displays components in a webhook message.
+"""
+
+
+__version__ = "0.0.5a"
+__author__ = "AWeirdScratcher"
+__email__ = "aweirdscratcher@gmail.com"
+__copyright__ = "Copyright 2021, AWeirdScratcher"
+
+__all__ = [
+    "button",
+    "URLbutton",
+    "checkWebhook",
+    "checkWebhookToken",
+    "send",
+    "embed"
+]
