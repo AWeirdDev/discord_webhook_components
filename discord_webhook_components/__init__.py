50832215810b4f2c42550599b4801704c7faf498
AWeirdScratcher <90096971+AWeirdScratcher@users.noreply.github.com>
2021-12-26 19:02:59 +0800

    Update __init__.py

--- discord_webhook_components/__init__.py
+++ discord_webhook_components/__init__.py
@@ -5,7 +5,7 @@ discord_webhook_components is a module that displays components in a webhook mes
 """
 
 
-__version__ = "0.0.5a"
+__version__ = "0.0.1"
 __author__ = "AWeirdScratcher"
 __email__ = "aweirdscratcher@gmail.com"
 __copyright__ = "Copyright 2021, AWeirdScratcher"
