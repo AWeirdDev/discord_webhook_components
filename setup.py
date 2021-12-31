f8ba9702d0278077b30db2d281bbc19692290b72
AWeirdScratcher <90096971+AWeirdScratcher@users.noreply.github.com>
2021-12-31 19:07:41 +0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='TowelStuff',
+    version='0.0.2',
+    packages=['discord_webhook_components'],
+    license='MIT license',
+    long_description='discord_webhook_components is a module that displays buttons in a webhook message',
+)
